1158f4e761fa127464ae923da35f3ae789bd7fb0
Brianne <leathbk1512@gmail.com>
2024-03-23 17:38:19 -0600

    Added generic welcome page with navigation bar

--- business_app/views.py
+++ business_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    # Render the HTML template index.html with the
+    # data in the context variable.
+    return render ( request, 'business_app/index.html')
+
+
+# example for a second login page with a basic placeholder response
+def login(request):
+    return render (HttpResponse, 'Login Page')
